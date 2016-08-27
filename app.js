61e712a89369079b0dd3a1fee9559ef37279c40e
Jashon Brown <jashon.brown@gmail.com>
2016-08-27 18:18:33 +1200

    Did basic conversion for a few html files from old site. Semi working pages: Fluids, Personal, Login

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var dataEntry = require('./routes/data_entry');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/data_entry', dataEntry)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
